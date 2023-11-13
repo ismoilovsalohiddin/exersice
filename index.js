950ad5112394c0b403cf73d5a60d0e1ac9d183a4
ismoilovsalohiddin <ismailovsalohiddin8@gmail.com>
2023-11-13 22:35:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// let userName = prompt("Ismingizni kiriting...");
+// let age =prompt("Yoshingizni kiriting...")
+// if(age>=18){
+//     alert(`${userName} uylanish vakti keldi`)
+// }else{
+//     alert(`${userName} azgina sabr kiling`)
+// }
+// const user = {
+//     name: "kiritilmagan",
+//     age: "kiritilmagan",
+//     address: "kiritilmagan",
+//     isSingle: false
+//   }
+  
+// user.name= prompt("Ismingizni kiriting...")
+// user.age = prompt("Yoshingizni kiriting...")
+// user.address = prompt("Addressingizni kiriting...")
+// user.isSingle = confirm("Uylanganmisiz?")
+
+
+// if(user.isSingle)  {
+//     user.isSingle="Ha"
+// }else{
+//     user.isSingle="Yok"
+// }
+// console.log(`Name: ${user.name}`)
+// console.log(`Age: ${user.age}`)
+// console.log(`Address: ${user.address}`)
+// console.log(`Married: ${user.isSingle}`)
+
+// let birthYear = prompt("yilingizni kiriting...")
+
+// console.log(2023-birthYear)
+
+
+  
+
